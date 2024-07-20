44158a996a1103f99b13914954247a51b6e96a02
lostmart <martinpedraza1979@hmail.com>
2024-07-20 22:27:38 +0200

    remove tests

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = 3000
 
 // Define a route handler for the default home page
 app.get("/", (req, res) => {
-    res.json({
-        msg: "el ninja esta cercano"
-    })
+	res.json({
+		msg: "el ninja esta cercano",
+	})
 })
 
 // Start the server
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening  at http://localhost:${port}`)
 })
